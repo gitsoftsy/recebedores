22c842f74a5316ba3c47018d2f8bbd7f756e3e64
viniciussoftsy <168453902+viniciussoftsy@users.noreply.github.com>
2025-03-18 08:33:03 -0300

    Correção de merge

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -3,8 +3,10 @@ import { useContext } from "react";
 
 export function useUser() {
   const context = useContext(UserContext);
+
   if (!context) {
     throw new Error("useUser must be used within a UserProvider");
   }
   return context;
+  
 }
